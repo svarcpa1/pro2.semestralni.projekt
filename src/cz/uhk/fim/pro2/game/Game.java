ffb99e8fd2fbbd82bf45daec547f399db54c78e3
paja <paja@pc-pavel.mshome.net>
2016-12-17 15:18:51 +0100

    PŘIDÁ UKLÁDÁNÍ DO SOUBORU

--- src/cz/uhk/fim/pro2/game/Game.java
+++ src/cz/uhk/fim/pro2/game/Game.java
@@ -1,13 +1,27 @@
 package cz.uhk.fim.pro2.game;
 
+import java.io.File;
+import java.io.IOException;
+
 import cz.uhk.fim.game.gui.HomeScreen;
 import cz.uhk.fim.game.gui.MainFrame;
 import cz.uhk.fim.game.gui.ScoreScreen;
 
 
 public class Game {
+	
+	public static final String SCORE_FILE = "scores.csv";
 
 	public static void main(String[] args) {
+		
+	File file = new File(SCORE_FILE);
+		if(!file.exists()){
+			try {
+				file.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+			}
+		}
 
 		MainFrame mainFrame = new MainFrame();
 		
--- src/cz/uhk/fim/pro2/game/ScoreManager.java
+++ src/cz/uhk/fim/pro2/game/ScoreManager.java
@@ -1,6 +1,11 @@
 package cz.uhk.fim.pro2.game;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.xml.bind.SchemaOutputResolver;
@@ -12,9 +17,42 @@ public class ScoreManager {
 		this.scoreList = new ArrayList<Integer>();
 	}	
 	public void addScore(int score){
+		List<Integer> scoreList = getAll();
 		scoreList.add(score);
+		try {
+			FileWriter fileWriter = new FileWriter(Game.SCORE_FILE);
+			 		
+				for (int value : scoreList) {
+					fileWriter.append(String.valueOf(value));
+					fileWriter.append(";");
+					fileWriter.append(new Date().toGMTString());
+					fileWriter.append(";");
+					fileWriter.append("Jmeno");
+					fileWriter.append("\n");
+				}
+				fileWriter.flush();
+				fileWriter.close();
+			} catch (IOException e) {
+				System.out.println("Chyba pri zapisovani");
+					}
+			scoreList.add(score);
 	}
 	public List<Integer> getAll(){
+		List<Integer> scoreList = new ArrayList<>();
+				
+				try {
+					FileReader fileReader = new FileReader(Game.SCORE_FILE);
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					
+					String line;
+					while ((line = bufferedReader.readLine()) != null) {
+						String[] values = line.split(";");
+						scoreList.add(Integer.valueOf(values[0]));				
+					}
+					
+				} catch (Exception e) {
+					System.out.println("Chyba pri nacitani");
+				}
 		return scoreList;
 	}
 	
